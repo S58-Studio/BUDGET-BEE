86fdc5cbf6c1e69c1b3cbf2f85b494701b4412a4
ROTICH <shaunmurphyx5@gmail.com>
2025-02-09 11:17:04 +0330

    Gradle and Libraries

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    // Run with:
+    // ./gradlew detekt // Simple report in the console
+    // ./gradlew detektFormat // To check with enabled auto-correction
+    id("financeAndMoney.detekt")
+    id("com.jraska.module.graph.assertion")
+
+    alias(libs.plugins.gradleWrapperUpgrade)
+    alias(libs.plugins.koverPlugin)
+
+}
+
+subprojects {
+    apply(plugin = "org.jetbrains.kotlinx.kover")
+    kover {
+        reports {
+            filters {
+                excludes {
+                    classes(
+                        "*Activity",
+                        "*Activity\$*",
+                        "*.BuildConfig",
+                        "dagger.hilt.*",
+                        "hilt_aggregated_deps.*",
+                        "*.Hilt_*"
+                    )
+                    annotatedBy("@Composable")
+                }
+            }
+        }
+    }
+}
+
+wrapperUpgrade {
+    gradle {
+        create("MySave") {
+            repo.set("Ivy-Apps/financeAndMoney-expenseAndBudgetPlanner")
+            baseBranch.set("main")
+        }
+    }
+}
